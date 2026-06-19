7f6174c5c51747cda4decd0b13b88ff2dd21ebb8
festusrono <festusrono56@gmail.com>
2026-06-19 09:48:06 +0300

    initial commit

---
+++ src/main/java/com/smoothcode/projectassigment/ProjectassigmentApplication.java
@@ -0,0 +1,13 @@
+package com.smoothcode.projectassigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectassigmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectassigmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoothcode/projectassigment/ProjectassigmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothcode.projectassigment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectassigmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
