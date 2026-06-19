572f411b2e1074dedfb0b4501c6c7b1a5d5af19c
festusrono <festusrono56@gmail.com>
2026-06-19 17:51:48 +0300

    apis updates on add and get all

--- src/main/java/com/smoothcode/projectassigment/StudentController.java
+++ src/main/java/com/smoothcode/projectassigment/StudentController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @CrossOrigin(origins = "${SPRING_ORIGINS:*}")
 @RequestMapping("/students")
 public class StudentController {
+    private static int maxProjectsPerStudent = 3;
     @Autowired
     private StudentRepository studentRepository;
     @Autowired
@@ -28,6 +29,12 @@ public class StudentController {
     public Student retrieveStudentById(int id ) {
         return studentRepository.findById(id).orElseThrow(
                 ()-> new ExpressionException("Student with id "+id+" is not found")
+        );
+    }
+
+    public Project retrieveProjectById(int id ) {
+        return projectRepository.findById(id).orElseThrow(
+                () -> new ExpressionException("Project with id " + id + " is not found")
         );
     }
     @GetMapping("/{id}")
@@ -44,6 +51,45 @@ public class StudentController {
         Student student = retrieveStudentById(id);
         student.setName(studentDetails.getName());
         student.setAverage(studentDetails.getAverage());
+        return ResponseEntity.ok(studentRepository.save(student));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable int id) {
+        Student student = retrieveStudentById(id);
+        studentRepository.delete(student);
+        return ResponseEntity.ok("Student Deleted Successfully");
+    }
+    @GetMapping("/max_project")
+    public ResponseEntity<Integer> getMaxProjects() {
+        return ResponseEntity.ok(maxProjectsPerStudent);
+    }
+    @PutMapping("/max_project")
+    public ResponseEntity<Integer> updateMaxProjectPerStudent(@RequestBody int maximumNumber) {
+        maxProjectsPerStudent = maximumNumber;
+        return ResponseEntity.ok(maxProjectsPerStudent);
+    }
+
+    @PostMapping("/{student_id}/projects/{project_id}")
+    public ResponseEntity<Student> addProjectToStudent(@PathVariable int student_id, @PathVariable int project_id) {
+        Student student = retrieveStudentById(student_id);
+        Project project = retrieveProjectById(project_id);
+        for (Project P : student.getProjects()) {
+            if (P.getId() == project.getId()) {
+                return ResponseEntity.status(400).body(student);
+            }
+        }
+        if (student.getProjects().size()>=maxProjectsPerStudent){
+            return ResponseEntity.badRequest().body(student);
+        }
+        student.getProjects().add(project);
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentRepository.save(student));
+    }
+
+    @DeleteMapping("/{student_id}/projects/{project_id}")
+    public ResponseEntity<Student> deleteProjectToStudent(@PathVariable int student_id, @PathVariable int project_id) {
+        Student student = retrieveStudentById(student_id);
+        Project project = retrieveProjectById(project_id);
+        student.getProjects().remove(project);
         return ResponseEntity.ok(studentRepository.save(student));
     }
 }
