507fcdfc7dc116fa1a975deef6601ae0b866861c
festusrono <festusrono56@gmail.com>
2026-06-19 15:49:15 +0300

    APIs retrievals updates

--- src/main/java/com/smoothcode/projectassigment/StudentController.java
+++ src/main/java/com/smoothcode/projectassigment/StudentController.java
@@ -6,6 +6,7 @@ import com.smoothcode.projectassigment.repository.ProjectRepository;
 import com.smoothcode.projectassigment.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.expression.ExpressionException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,13 +24,26 @@ public class StudentController {
     public ResponseEntity<List<Student>> getAllStudents() {
         return ResponseEntity.ok(studentRepository.findAll());
     }
-    public Project retrieveProjectById(int id ) {
-        return projectRepository.findById(id).orElseThrow(
-                ()-> new ExpressionException("Project with id "+id+" is not found")
+
+    public Student retrieveStudentById(int id ) {
+        return studentRepository.findById(id).orElseThrow(
+                ()-> new ExpressionException("Student with id "+id+" is not found")
         );
     }
     @GetMapping("/{id}")
     public ResponseEntity<Student> getStudentById(@PathVariable int id) {
         return ResponseEntity.of(studentRepository.findById(id));
+    }
+    @PostMapping("/")
+    public ResponseEntity<Student> createStudent(@RequestBody Student studentDetails) {
+        Student savedStudent = studentRepository.save(studentDetails);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedStudent);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student studentDetails) {
+        Student student = retrieveStudentById(id);
+        student.setName(studentDetails.getName());
+        student.setAverage(studentDetails.getAverage());
+        return ResponseEntity.ok(studentRepository.save(student));
     }
 }
