7c21f7d07daeb86e8bbcb0f58319649586c00e83
festusrono <festusrono56@gmail.com>
2026-06-19 12:15:08 +0300

    Adding projectController to the controller in the project

--- src/main/java/com/smoothcode/projectassigment/controller/ProjectController.java
+++ src/main/java/com/smoothcode/projectassigment/controller/ProjectController.java
@@ -1,11 +1,39 @@
 package com.smoothcode.projectassigment.controller;
 
+import com.smoothcode.projectassigment.model.Project;
 import com.smoothcode.projectassigment.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "${SPRING_ORIGINS:*}")
+@RequestMapping("/projects")
 
 public class ProjectController {
     @Autowired
     private ProjectRepository projectRepository;
 
-
+    @GetMapping("/")
+    public ResponseEntity<List<Project>> getAllProjects() {
+        return ResponseEntity.ok(projectRepository.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Project> getProjectById(@PathVariable int id) {
+        return ResponseEntity.of(projectRepository.findById(id));
+    }
+    @PostMapping("/")
+    public ResponseEntity<Project> createProject(@RequestBody Project projectDetails) {
+        Project project = projectRepository.save(projectDetails);
+        return ResponseEntity.status(HttpStatus.CREATED).body(project);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Project> updateProject(@PathVariable int id, @RequestBody Project projectDetails) {
+        Project project = projectRepository.findById(id).orElseThrow();
+        project.setName(projectDetails.getName());
+        return ResponseEntity.ok(projectRepository.save(project));
+    }
 }
